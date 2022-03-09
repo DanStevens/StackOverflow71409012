5c301663570dadbe2c09462f1f05913597d0e95e
Dan Stevens <Daniel.STEVENS@oxinst.com>
2022-03-09 14:06:58 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StackOverflow71409012")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oxford Instruments")]
+[assembly: AssemblyProduct("StackOverflow71409012")]
+[assembly: AssemblyCopyright("Copyright © Oxford Instruments 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bd4722b-543c-4ac9-9656-ab5c6b56e7eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ValuePusher.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Microsoft.Reactive.Testing;
+
+namespace StackOverflow71409012
+{
+    public sealed class ValuePusher : IDisposable
+    {
+        private readonly ValueReceiver _receiver;
+        private readonly IScheduler _scheduler;
+        private readonly Subject<int> _subject;
+
+        private int _value;
+
+        public ValuePusher(ValueReceiver receiver, IScheduler scheduler)
+        {
+            _receiver = receiver;
+            _scheduler = scheduler;
+
+            // Arrange to push values to `receiver` dependency
+            _subject = new Subject<int>();
+            _subject.ObserveOn(_scheduler)
+                //.Sample(TimeSpan.FromMilliseconds(50), _scheduler)
+                .SubscribeOn(_scheduler)
+                .Subscribe(i => PushCurrentValueToReceiver());
+        }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                _subject.OnNext(0);
+            }
+        }
+
+        private void PushCurrentValueToReceiver()
+        {
+            _receiver.Value = Value;
+        }
+
+        public void Dispose()
+        {
+            _subject?.OnCompleted();
+            _subject?.Dispose();
+        }
+    }
+
+    public class ValueReceiver
+    {
+        public int Value { get; set; }
+    }
+
+    [TestClass]
+    public class ValuePusherTests
+    {
+        [TestMethod]
+        [Timeout(1000)]
+        public void ReceiverReceivesValueFromPusherViaScheduler()
+        {
+            var scheduler = new TestScheduler();
+            var receiver = new ValueReceiver();
+
+            using (var pusher = new ValuePusher(receiver, scheduler))
+            {
+                scheduler.Start();
+                pusher.Value = 1;
+                scheduler.AdvanceBy(1);
+                Assert.AreEqual(1, receiver.Value);
+            }
+        }
+    }
+}
